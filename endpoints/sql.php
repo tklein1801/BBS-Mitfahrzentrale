c5f9d258f141c97ce25bd3d13a45302e805f41a8
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-02-24 12:22:38 +0100

    Update sql.php

--- endpoints/sql.php
+++ endpoints/sql.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("185.223.31.112", "db_thorben", "Litza123", "bbs-mitfahrzentrale-v2");
+$con = new mysqli("HOST", "USERNAME", "PASSWORD", "bbs-mitfahrzentrale");
 $con->set_charset("utf8");
 if($con->connect_error) {
 	echo "Connection to database failed. Reason: <br>";
